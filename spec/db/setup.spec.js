19f672650100d4ba9862f30024a225a61da7b90c
Adam Gray <adam@pateo.co>
2018-02-17 19:35:17 +0000

    Create db setup script

---
+++ spec/db/setup.spec.js
@@ -0,0 +1,9 @@
+const setup = require('../../src/db/setup')
+
+describe('DB - setup', () => {
+
+  it('should be defined', () => {
+    expect(setup).toBeDefined
+  })
+
+})
---
+++ src/db/setup.js
@@ -0,0 +1,26 @@
+const sequelize = require('./')
+const object = require('../../src/models/object')
+const page = require('../../src/models/page')
+const fieldInput = require('../../src/models/fieldInput')
+const content = require('../../src/models/content')
+const field = require('../../src/models/field')
+const fieldValue = require('../../src/models/fieldValue')
+
+return object.sync({ force: true })
+  .then(() => page.sync({ force: true }))
+  .then(() => fieldInput.sync({ force: true }))
+  .then(() => content.sync({ force: true }))
+  .then(() => field.sync({ force: true }))
+  .then(() => fieldValue.sync({ force: true }))
+  .then(() => {
+    return fieldInput.create({
+      title: 'Text',
+      type: 'text'
+    })
+  })
+  .then(() => {
+    console.log('Setup complete.')
+    sequelize.close()
+  })
+  .catch((error) => console.log(error))
+
