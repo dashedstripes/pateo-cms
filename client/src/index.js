da3945125782be569f1570a0fcd097654914c3df
Adam Gray <adam@pateo.co>
2018-04-05 10:42:34 +0100

    Add PageNotFound route

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import store from './store'
 
 import Home from './pages/Home'
@@ -22,26 +22,32 @@ import EditPage from './pages/Pages/EditPage'
 
 import PagesContents from './pages/PagesContents'
 
+import PageNotFound from './pages/PageNotFound'
+
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
       <Layout>
-        <Route exact path="/" component={Home} />
+        <Switch>
+          <Route exact path="/" component={Home} />
 
-        <Route exact path="/objects" component={Objects} />
-        <Route exact path="/objects/new" component={NewObject} />
-        <Route exact path="/objects/:object_id/edit" component={EditObject} />
+          <Route exact path="/objects" component={Objects} />
+          <Route exact path="/objects/new" component={NewObject} />
+          <Route exact path="/objects/:object_id/edit" component={EditObject} />
 
-        <Route exact path="/objects/:object_id/contents" component={ObjectContents} />
+          <Route exact path="/objects/:object_id/contents" component={ObjectContents} />
 
-        <Route exact path="/objects/:object_id/contents/new" component={NewContent} />
-        <Route exact path="/contents/:content_id/edit" component={EditContent} />
+          <Route exact path="/objects/:object_id/contents/new" component={NewContent} />
+          <Route exact path="/contents/:content_id/edit" component={EditContent} />
 
-        <Route exact path="/pages" component={Pages} />
-        <Route exact path="/pages/new" component={NewPage} />
-        <Route exact path="/pages/:page_id/edit" component={EditPage} />
+          <Route exact path="/pages" component={Pages} />
+          <Route exact path="/pages/new" component={NewPage} />
+          <Route exact path="/pages/:page_id/edit" component={EditPage} />
 
-        <Route exact path="/pages/:page_id/contents" component={PagesContents} />
+          <Route exact path="/pages/:page_id/contents" component={PagesContents} />
+
+          <Route exact path="*" component={PageNotFound} />
+        </Switch>
       </Layout>
     </BrowserRouter>
   </Provider>,
