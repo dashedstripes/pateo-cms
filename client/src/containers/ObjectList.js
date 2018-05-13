f862ab0b103abad78a452a592737726f11cbe546
Adam Gray <adam@pateo.co>
2018-05-13 11:44:31 +0100

    Restart object list with hardcoded data

--- client/src/containers/ObjectList.js
+++ client/src/containers/ObjectList.js
@@ -1,46 +1,65 @@
 import React, { Component } from 'react'
-import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 
-import { fetchObjects } from '../actions/objectActions'
-
-import ObjectListItem from '../components/ObjectListItem'
-
 class ObjectList extends Component {
-  componentDidMount() {
-    this.props.dispatch(fetchObjects())
+  constructor(props) {
+    super(props)
+    this.state = {
+      objects: [
+        {
+          id: 1,
+          title: 'Artwork'
+        },
+        {
+          id: 2,
+          title: 'Property'
+        }
+      ]
+    }
+  }
+
+  handleDelete(id) {
+    this.setState({
+      objects: this.state.objects.filter((object) => object.id !== id)
+    })
   }
 
   render() {
-
-    let objects = this.props.objects.map((object) =>
-      <ObjectListItem key={object.id} id={object.id} title={object.title} />)
-
-    if (this.props.error.message) {
-      return (
-        <div>
-          <p>{this.props.error.message}</p>
+    let objects = this.state.objects.map((object) => (
+      <div key={object.id} class='col-4 py-3'>
+        <div class='card'>
+          <div class='card-body'>
+            <h5 class='card-title'>{object.title}</h5>
+            <div class='btn-group'>
+              <Link class='btn btn-link' to={'/objects/' + object.id + '/edit'}>
+                <i class="far fa-edit"></i>
+              </Link>
+              <button class='btn btn-link' onClick={this.handleDelete.bind(this, object.id)}>
+                <i class="fas fa-trash"></i>
+              </button>
+            </div>
+          </div>
         </div>
-      )
-    }
+      </div>
+    ))
 
     return (
       <div>
-        <h1>Objects</h1>
-        <Link to='/objects/new'>New Object</Link>
-        {objects}
+        <div class='row py-4'>
+          <div class='col-6'>
+            <h3>Objects</h3>
+          </div>
+          <div class='col-6'>
+            <Link class='btn btn-primary float-right' to='/objects/new'>New Object</Link>
+          </div>
+        </div>
+        <div class='row'>
+          {objects}
+        </div>
       </div>
     )
 
   }
 }
 
-function mapStateToProps(state) {
-  return {
-    pending: state.objects.pending,
-    error: state.objects.error,
-    objects: state.objects.objects
-  }
-}
-
-export default connect(mapStateToProps)(ObjectList)
+export default ObjectList
