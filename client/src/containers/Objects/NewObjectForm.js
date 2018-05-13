acf1bdde7623c4df22c8d21ecb61042aee071139
Adam Gray <adam@pateo.co>
2018-05-13 17:33:07 +0100

    PostObjectAndFields on NewObjectForm save

--- client/src/containers/Objects/NewObjectForm.js
+++ client/src/containers/Objects/NewObjectForm.js
@@ -1,40 +1,57 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+
+const defaultState = {
+  title: 'Artwork',
+  slug: 'artwork',
+  fields: [
+    {
+      id: 1,
+      title: 'description',
+      type: 'text'
+    },
+    {
+      id: 2,
+      title: 'floorspace',
+      type: 'number'
+    }
+  ],
+  fieldInputs: [
+    {
+      id: 1,
+      title: 'Text',
+      type: 'text'
+    },
+    {
+      id: 2,
+      title: 'Number',
+      type: 'number'
+    }
+  ]
+}
 
 class NewObjectForm extends Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      title: 'Artwork',
-      slug: 'artwork',
-      fields: [
-        {
-          id: 1,
-          title: 'description',
-          type: 'text'
-        },
-        {
-          id: 2,
-          title: 'floorspace',
-          type: 'number'
-        }
-      ],
-      fieldInputs: [
-        {
-          id: 1,
-          title: 'Text',
-          type: 'text'
-        },
-        {
-          id: 2,
-          title: 'Number',
-          type: 'number'
-        }
-      ]
+      isLoading: false,
+      title: '',
+      slug: '',
+      fields: [],
+      fieldInputs: []
     }
 
     this.handleAddField = this.handleAddField.bind(this)
     this.handleTitleChange = this.handleTitleChange.bind(this)
+    this.handleSave = this.handleSave.bind(this)
+  }
+
+  componentDidMount() {
+    // Get the field inputs from database and set them in the state object
+    axios.get('/api/field_inputs')
+      .then((res) => this.setState({ fieldInputs: res.data }))
+      .catch((err) => console.log(error))
   }
 
   // This also generates a slug based on the title
@@ -75,6 +92,43 @@ class NewObjectForm extends Component {
     })
   }
 
+  postObjectAndFields() {
+    // Because we have an unknown amount of fields, we will need to store each post request
+    // into an array, then use Promise.all to post them all async.
+    let fieldPromises = []
+
+    // Create object in the database and retrieve the ID to create fields
+    axios.post('/api/objects', {
+      title: this.state.title
+    }).then((res) => {
+      this.state.fields.map((field) => {
+        fieldPromises.push(
+          axios.post('/api/fields', {
+            title: field.title,
+            objectId: res.data.id,
+            // We've got to get the ID for the selected fieldInput for this field.
+            // We loop through each fieldInput, and filter down to the one that matches the type set on the 
+            // current field, as .filter returns an array, we get the item at [0] and grab the id.
+            fieldInputId: this.state.fieldInputs.filter((fieldInput) => fieldInput.type === field.type)[0].id
+          })
+        )
+      })
+
+      return Promise.all(fieldPromises)
+    })
+      .then((res) => this.setState({ isLoading: false }))
+      .catch((err) => this.setState({ isLoading: false }))
+  }
+
+  handleSave() {
+    // Set State to loading
+    this.setState({
+      isLoading: true
+    })
+
+    this.postObjectAndFields()
+  }
+
   render() {
     let fields = this.state.fields.map((field) => {
       return (
@@ -107,7 +161,7 @@ class NewObjectForm extends Component {
           <div class='col-6'>
             <div class='btn-group float-right'>
               <button class='btn btn-primary' onClick={this.handleAddField}>Add Field</button>
-              <button class='btn btn-success' disabled={this.state.title === ''}>Save</button>
+              <button class='btn btn-success' onClick={this.handleSave} disabled={this.state.title === ''}>Save</button>
             </div>
           </div>
         </div>
