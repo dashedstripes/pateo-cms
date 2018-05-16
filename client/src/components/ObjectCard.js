cf58cdb10e89a552349e8e70e1c8ba0cbc15a03d
Adam Gray <adam@pateo.co>
2018-05-16 13:33:35 +0100

    Clean up ObjectCard UI

--- client/src/components/ObjectCard.js
+++ client/src/components/ObjectCard.js
@@ -3,9 +3,13 @@ import { Link } from 'react-router-dom'
 
 const ObjectCard = ({ id, title, slug, onDelete }) => (
   <div class='card'>
-    <div class='card-body'>
-      <h5 class='card-title'>{title}</h5>
-      <p class='card-subtitle text-muted'>{slug}</p>
+    <div class='card-body align-items-center'>
+      <div class='float-left'>
+        <h6 class='card-title mb-1'>{title}</h6>
+        <p class='card-subtitle text-muted'>
+          <small>{slug}</small>
+        </p>
+      </div>
       <div class='btn-group float-right'>
         <Link class='btn btn-link' to={'/objects/' + id + '/edit'}>
           <i class="far fa-edit"></i>
