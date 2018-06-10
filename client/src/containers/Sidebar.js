4833fab3c666d38926491a66fa83b670c766c8ae
Adam Gray <adam@pateo.co>
2018-06-10 13:49:20 +0100

    Add something to the dashboard page and add active states to sidebar

--- client/src/containers/Sidebar.js
+++ client/src/containers/Sidebar.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 class Sidebar extends Component {
   render() {
@@ -7,19 +7,30 @@ class Sidebar extends Component {
       <div>
         <div class='py-3'>
           <div>
-            <h5>
-              <h5 class='sidebar-heading'>admin</h5>
-            </h5>
+            <h5 class='sidebar-heading'>pateo cms</h5>
 
             <div class='list-group list-group-hover'>
-              <Link class='list-group-item sidebar-list-item sidebar-list-item list-group-item-action active' to='/objects'>
+              <NavLink class='list-group-item sidebar-list-item sidebar-list-item list-group-item-action' exact activeClassName="active" to='/'>
+                <i class="fas fa-tachometer-alt fa-fw sidebar-icon"></i>
+                dashboard
+              </NavLink>
+            </div>
+          </div>
+
+          <hr />
+
+          <div>
+            <h5 class='sidebar-heading'>admin</h5>
+
+            <div class='list-group list-group-hover'>
+              <NavLink class='list-group-item sidebar-list-item sidebar-list-item list-group-item-action' exact activeClassName="active" to='/objects/'>
                 <i class="fas fa-box-open fa-fw sidebar-icon"></i>
                 objects
-              </Link>
-              <Link class='list-group-item sidebar-list-item sidebar-list-item list-group-item-action' to='/pages'>
+              </NavLink>
+              <NavLink class='list-group-item sidebar-list-item sidebar-list-item list-group-item-action' exact activeClassName="active" to='/pages'>
                 <i class="fas fa-file fa-fw sidebar-icon"></i>
                 pages
-              </Link>
+              </NavLink>
             </div>
           </div>
 
@@ -28,14 +39,14 @@ class Sidebar extends Component {
           <div>
             <h5 class='sidebar-heading'>objects</h5>
             <div class='list-group'>
-              <Link class='list-group-item sidebar-list-item sidebar-list-item list-group-item-action' to='/objects/1/contents'>
+              <NavLink class='list-group-item sidebar-list-item sidebar-list-item list-group-item-action' exact activeClassName="active" to='/objects/1/contents'>
                 <i class="fas fa-box-open fa-fw sidebar-icon"></i>
                 artwork
-              </Link>
-              <Link class='list-group-item sidebar-list-item sidebar-list-item list-group-item-action' to='/objects/2/contents'>
+              </NavLink>
+              <NavLink class='list-group-item sidebar-list-item sidebar-list-item list-group-item-action' exact activeClassName="active" to='/objects/2/contents'>
                 <i class="fas fa-box-open fa-fw sidebar-icon"></i>
                 property
-              </Link>
+              </NavLink>
             </div>
           </div>
 
@@ -44,14 +55,14 @@ class Sidebar extends Component {
           <div>
             <h5 class='sidebar-heading'>pages</h5>
             <div class='list-group'>
-              <Link class='list-group-item sidebar-list-item sidebar-list-item list-group-item-action' to='/pages/1/contents'>
+              <NavLink class='list-group-item sidebar-list-item sidebar-list-item list-group-item-action' exact activeClassName="active" to='/pages/1/contents'>
                 <i class="fas fa-file fa-fw sidebar-icon"></i>
                 about
-              </Link>
-              <Link class='list-group-item sidebar-list-item sidebar-list-item list-group-item-action' to='/pages/2/contents'>
+              </NavLink>
+              <NavLink class='list-group-item sidebar-list-item sidebar-list-item list-group-item-action' exact activeClassName="active" to='/pages/2/contents'>
                 <i class="fas fa-file fa-fw sidebar-icon"></i>
                 contact
-              </Link>
+              </NavLink>
             </div>
           </div>
         </div>
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,12 +3,44 @@ import React, { Component } from 'react'
 class Home extends Component {
   constructor() {
     super()
+
+    this.state = {
+      isLoading: true,
+      objects: [],
+      pages: []
+    }
   }
+
+  componentDidMount() {
+    Promise.all([
+      fetch('/api/objects'),
+      fetch('/api/pages')
+    ]).then((res) => {
+      return Promise.all(res.map((r) => r.json()))
+    }).then((res) => {
+      let objects = res[0]
+      let pages = res[1]
+
+      this.setState({
+        isLoading: false,
+        objects,
+        pages
+      })
+    })
+  }
+
   render() {
+
+    if (this.state.isLoading) {
+      <div>
+        <h1>Dashboard</h1>
+      </div>
+    }
+
     return (
       <div>
-        <h1>Pateo CMS</h1>
-        <p>Homepage</p>
+        <h1>Dashboard</h1>
+        <p>You have {this.state.objects.length} objects, and {this.state.pages.length} pages.</p>
       </div>
     )
   }
