66ac750d217b8ff8b39ff08db422b5e00ec63581
Adam Gray <adam@pateo.co>
2018-06-10 08:55:46 +0100

    Style more of the sidebar

--- client/src/containers/Sidebar.js
+++ client/src/containers/Sidebar.js
@@ -4,37 +4,55 @@ import { Link } from 'react-router-dom'
 class Sidebar extends Component {
   render() {
     return (
-      <div class='py-3'>
-        <div class='mb-5'>
-          <h5>
-            <Link class='sidebar-heading' to='/'>pateo cms</Link>
-          </h5>
+      <div>
+        <div class='py-3'>
+          <div>
+            <h5>
+              <h5 class='sidebar-heading'>admin</h5>
+            </h5>
 
-          <div class='list-group list-group-hover'>
-            <Link class='list-group-item sidebar-list-item list-group-item-action' to='/objects'>
-              <span class="badge badge-pill"><i class="fas fa-box-open"></i></span>
-              objects
-            </Link>
-            <Link class='list-group-item sidebar-list-item list-group-item-action' to='/pages'>
-              <span class="badge badge-pill"><i class="fas fa-file"></i></span>
-              pages
-            </Link>
+            <div class='list-group list-group-hover'>
+              <Link class='list-group-item sidebar-list-item sidebar-list-item list-group-item-action active' to='/objects'>
+                <i class="fas fa-box-open fa-fw sidebar-icon"></i>
+                objects
+              </Link>
+              <Link class='list-group-item sidebar-list-item sidebar-list-item list-group-item-action' to='/pages'>
+                <i class="fas fa-file fa-fw sidebar-icon"></i>
+                pages
+              </Link>
+            </div>
           </div>
-        </div>
 
-        <div class='mb-5'>
-          <h5 class='sidebar-heading'>objects</h5>
-          <div class='list-group'>
-            <Link class='list-group-item sidebar-list-item list-group-item-action' to='/objects/1/contents'>artwork</Link>
-            <Link class='list-group-item sidebar-list-item list-group-item-action' to='/objects/2/contents'>property</Link>
+          <hr />
+
+          <div>
+            <h5 class='sidebar-heading'>objects</h5>
+            <div class='list-group'>
+              <Link class='list-group-item sidebar-list-item sidebar-list-item list-group-item-action' to='/objects/1/contents'>
+                <i class="fas fa-box-open fa-fw sidebar-icon"></i>
+                artwork
+              </Link>
+              <Link class='list-group-item sidebar-list-item sidebar-list-item list-group-item-action' to='/objects/2/contents'>
+                <i class="fas fa-box-open fa-fw sidebar-icon"></i>
+                property
+              </Link>
+            </div>
           </div>
-        </div>
 
-        <div>
-          <h5 class='sidebar-heading'>pages</h5>
-          <div class='list-group'>
-            <Link class='list-group-item sidebar-list-item list-group-item-action' to='/pages/1/contents'>about</Link>
-            <Link class='list-group-item sidebar-list-item list-group-item-action' to='/pages/2/contents'>contact</Link>
+          <hr />
+
+          <div>
+            <h5 class='sidebar-heading'>pages</h5>
+            <div class='list-group'>
+              <Link class='list-group-item sidebar-list-item sidebar-list-item list-group-item-action' to='/pages/1/contents'>
+                <i class="fas fa-file fa-fw sidebar-icon"></i>
+                about
+              </Link>
+              <Link class='list-group-item sidebar-list-item sidebar-list-item list-group-item-action' to='/pages/2/contents'>
+                <i class="fas fa-file fa-fw sidebar-icon"></i>
+                contact
+              </Link>
+            </div>
           </div>
         </div>
       </div>
